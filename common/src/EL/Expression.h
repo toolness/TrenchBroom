0b4f98345319ca2adadab1da5ef43b6547936ed9
Kristian Duske <kristian.duske@gmail.com>
2021-07-04 15:51:50 +0200

    2102: Extract concrete expression types into separate unit
    We need to extract these types into a separate unit in order to remove
    the Value.h include from Expression.h. This would cause a circular
    include in a following commit where we need to include Expression.h in
    Value.h

--- common/src/EL/Expression.h
+++ common/src/EL/Expression.h
@@ -21,14 +21,11 @@
 
 #include "Macros.h"
 #include "EL/EL_Forward.h"
-#include "EL/Value.h"
 
 #include <iosfwd>
-#include <map>
 #include <memory>
 #include <string>
 #include <variant>
-#include <vector>
 
 namespace TrenchBroom {
     namespace EL {
@@ -77,159 +74,6 @@ namespace TrenchBroom {
         private:
             void rebalanceByPrecedence();
             size_t precedence() const;
-        };
-        
-        class LiteralExpression {
-        private:
-            Value m_value;
-        public:
-            LiteralExpression(Value value);
-            
-            const Value& evaluate(const EvaluationContext& context) const;
-
-            friend bool operator==(const LiteralExpression& lhs, const LiteralExpression& rhs);
-            friend bool operator!=(const LiteralExpression& lhs, const LiteralExpression& rhs);
-            friend std::ostream& operator<<(std::ostream& str, const LiteralExpression& exp);
-        };
-        
-        class VariableExpression {
-        private:
-            std::string m_variableName;
-        public:
-            VariableExpression(std::string variableName);
-            
-            Value evaluate(const EvaluationContext& context) const;
-
-            friend bool operator==(const VariableExpression& lhs, const VariableExpression& rhs);
-            friend bool operator!=(const VariableExpression& lhs, const VariableExpression& rhs);
-            friend std::ostream& operator<<(std::ostream& str, const VariableExpression& exp);
-        };
-        
-        class ArrayExpression {
-        private:
-            std::vector<Expression> m_elements;
-        public:
-            ArrayExpression(std::vector<Expression> elements);
-            
-            Value evaluate(const EvaluationContext& context) const;
-            ExpressionVariant optimize() const;
-
-            friend bool operator==(const ArrayExpression& lhs, const ArrayExpression& rhs);
-            friend bool operator!=(const ArrayExpression& lhs, const ArrayExpression& rhs);
-            friend std::ostream& operator<<(std::ostream& str, const ArrayExpression& exp);
-        };
-        
-        class MapExpression {
-        private:
-            std::map<std::string, Expression> m_elements;
-        public:
-            MapExpression(std::map<std::string, Expression> elements);
-
-            Value evaluate(const EvaluationContext& context) const;
-            ExpressionVariant optimize() const;
-
-            friend bool operator==(const MapExpression& lhs, const MapExpression& rhs);
-            friend bool operator!=(const MapExpression& lhs, const MapExpression& rhs);
-            friend std::ostream& operator<<(std::ostream& str, const MapExpression& exp);
-        };
-        
-        enum class UnaryOperator {
-            Plus,
-            Minus,
-            LogicalNegation,
-            BitwiseNegation,
-            Group
-        };
-        
-        class UnaryExpression {
-        private:
-            UnaryOperator m_operator;
-            Expression m_operand;
-        public:
-            UnaryExpression(UnaryOperator i_operator, Expression operand);
-
-            Value evaluate(const EvaluationContext& context) const;
-            ExpressionVariant optimize() const;
-
-            friend bool operator==(const UnaryExpression& lhs, const UnaryExpression& rhs);
-            friend bool operator!=(const UnaryExpression& lhs, const UnaryExpression& rhs);
-            friend std::ostream& operator<<(std::ostream& str, const UnaryExpression& exp);
-        };
-        
-        enum class BinaryOperator {
-            Addition,
-            Subtraction,
-            Multiplication,
-            Division,
-            Modulus,
-            LogicalAnd,
-            LogicalOr,
-            BitwiseAnd,
-            BitwiseXOr,
-            BitwiseOr,
-            BitwiseShiftLeft,
-            BitwiseShiftRight,
-            Less,
-            LessOrEqual,
-            Greater,
-            GreaterOrEqual,
-            Equal,
-            NotEqual,
-            Range,
-            Case,
-        };
-        
-        class BinaryExpression {
-        public:
-            friend class Expression;
-        private:
-            BinaryOperator m_operator;
-            Expression m_leftOperand;
-            Expression m_rightOperand;
-        public:
-            BinaryExpression(BinaryOperator i_operator, Expression leftOperand, Expression rightOperand);
-            static Expression createAutoRangeWithRightOperand(Expression rightOperand, size_t line, size_t column);
-            static Expression createAutoRangeWithLeftOperand(Expression leftOperand, size_t line, size_t column);
-
-            Value evaluate(const EvaluationContext& context) const;
-            ExpressionVariant optimize() const;
-            
-            size_t precedence() const;
-
-            friend bool operator==(const BinaryExpression& lhs, const BinaryExpression& rhs);
-            friend bool operator!=(const BinaryExpression& lhs, const BinaryExpression& rhs);
-            friend std::ostream& operator<<(std::ostream& str, const BinaryExpression& exp);
-        };
-        
-        class SubscriptExpression {
-        public:
-            static const std::string& AutoRangeParameterName();
-        private:
-            Expression m_leftOperand;
-            Expression m_rightOperand;
-        public:
-            SubscriptExpression(Expression leftOperand, Expression rightOperand);
-            
-            Value evaluate(const EvaluationContext& context) const;
-            ExpressionVariant optimize() const;
-
-            friend bool operator==(const SubscriptExpression& lhs, const SubscriptExpression& rhs);
-            friend bool operator!=(const SubscriptExpression& lhs, const SubscriptExpression& rhs);
-            friend std::ostream& operator<<(std::ostream& str, const SubscriptExpression& exp);
-        };
-        
-        class SwitchExpression {
-        private:
-            std::vector<Expression> m_cases;
-        public:
-            SwitchExpression(std::vector<Expression> cases);
-
-            Value evaluate(const EvaluationContext& context) const;
-            ExpressionVariant optimize() const;
-
-            friend bool operator==(const SwitchExpression& lhs, const SwitchExpression& rhs);
-            friend bool operator!=(const SwitchExpression& lhs, const SwitchExpression& rhs);
-            friend std::ostream& operator<<(std::ostream& str, const SwitchExpression& exp);
         };
     }
 }
